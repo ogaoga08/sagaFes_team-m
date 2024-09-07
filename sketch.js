72c2f38d61e92fce7672191317148ce1db96c627
ogaoga08 <kamekameshin0514@gmail.com>
2024-09-07 23:45:02 +0900

    first commit

---
+++ sketch.js
@@ -0,0 +1,243 @@
+//Real Time Face Detection in p5.js from https://www.youtube.com/watch?v=3yqANLRWGLo
+
+let faceapi;
+let detections = [];
+
+let fireworks = [];
+let gravity;
+
+let video;
+let canvas;
+
+let gameStarted = false; // ゲームが開始されたかどうかのフラグ
+let happy = 0; // happinessの値を保存する変数
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+  colorMode(HSB);
+  gravity = createVector(0, 0.1);
+  stroke(255);
+  strokeWeight(4);
+  
+  canvas.id("canvas");
+
+  video = createCapture(VIDEO);// Create video
+  video.id("video");
+  video.size(width, height);
+
+  const faceOptions = {
+    withLandmarks: true,
+    withExpressions: true,
+    withDescriptors: true,
+    minConfidence: 0.5
+  };
+
+  // Initialize the model
+  faceapi = ml5.faceApi(video, faceOptions, faceReady);
+
+  // ゲームスタートボタンを作成
+  let startButton = createButton("Start Game");
+  startButton.position(20, 20);
+  startButton.mousePressed(() => {
+    gameStarted = true; // ゲームを開始
+    startButton.hide(); // ボタンを非表示にする
+  });
+}
+
+function faceReady() {
+  faceapi.detect(gotFaces);// Start detecting faces: 顔認識開始
+}
+
+// Get faces
+function gotFaces(error, result) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  detections = result;　//Now all the data in this detections: 
+
+  clear();//Draw transparent background
+  drawBoxs(detections);//Draw detection box:
+  drawLandmarks(detections);//// Draw all the face points
+  drawExpressions(detections, 20, 250, 14);//Draw face expression
+
+  faceapi.detect(gotFaces);// Call the function again here
+}
+
+function drawBoxs(detections){
+  if (detections.length > 0) {//If at least 1 face is detected: 
+    for (f=0; f < detections.length; f++){
+      let {_x, _y, _width, _height} = detections[f].alignedRect._box;
+      stroke(44, 169, 225);
+      strokeWeight(1);
+      noFill();
+      rect(_x, _y, _width, _height);
+    }
+  }
+}
+
+function drawLandmarks(detections){
+  if (detections.length > 0) {//If at least 1 face is detected
+    for (f=0; f < detections.length; f++){
+      let points = detections[f].landmarks.positions;
+      for (let i = 0; i < points.length; i++) {
+        stroke(44, 169, 225);
+        strokeWeight(3);
+        point(points[i]._x, points[i]._y);
+      }
+    }
+  }
+}
+
+function drawExpressions(detections, x, y, textYSpace){
+  if (detections.length > 0) { // If at least 1 face is detected
+    let {neutral, happy, angry, sad, disgusted, surprised, fearful} = detections[0].expressions;
+    textFont('Helvetica Neue');
+    textSize(14);
+    
+    noStroke();
+    fill(0);
+
+    text("neutral:       " + nf(neutral * 100, 2, 2) + "%", x, y);
+    text("happiness: " + nf(happy * 100, 2, 2) + "%", x, y + textYSpace);
+    text("anger:        " + nf(angry * 100, 2, 2) + "%", x, y + textYSpace * 2);
+    text("sad:            " + nf(sad * 100, 2, 2) + "%", x, y + textYSpace * 3);
+    text("disgusted: " + nf(disgusted * 100, 2, 2) + "%", x, y + textYSpace * 4);
+    text("surprised:  " + nf(surprised * 100, 2, 2) + "%", x, y + textYSpace * 5);
+    text("fear:           " + nf(fearful * 100, 2, 2) + "%", x, y + textYSpace * 6);
+
+    // happinessの値をグローバル変数に反映
+    happy = happy; 
+  } else { // If no faces are detected
+    text("neutral: ", x, y);
+    text("happiness: ", x, y + textYSpace);
+    text("anger: ", x, y + textYSpace * 2);
+    text("sad: ", x, y + textYSpace * 3);
+    text("disgusted: ", x, y + textYSpace * 4);
+    text("surprised: ", x, y + textYSpace * 5);
+    text("fear: ", x, y + textYSpace * 6);
+  }
+}
+
+function draw() {
+  if (!gameStarted) return; // ゲームが開始されていない場合は何もしない
+
+  if (random(1) < 0.03) {
+    fireworks.push(new Firework());
+  }
+
+  for (let i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+}
+
+class Firework {
+  constructor() {
+    this.firework = new Particle(random(width), height, true);
+    this.exploded = false;
+    this.particles = [];
+  }
+
+  done() {
+    return this.exploded && this.particles.length === 0;
+  }
+
+  update() {
+    if (!this.exploded) {
+      this.firework.applyForce(gravity);
+      this.firework.update();
+
+      // happinessが90%以上なら花火を爆発させる
+      if (this.firework.vel.y >= 0) {
+        if (happy * 100 >= 90) {
+          this.exploded = true;
+          this.explode();
+        } else {
+          this.exploded = true; // 爆発はするが、particlesを生成しない
+        }
+      }
+    }
+
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].applyForce(gravity);
+      this.particles[i].update();
+
+      if (this.particles[i].done()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  }
+
+  explode() {
+    for (let i = 0; i < 100; i++) {
+      let p = new Particle(this.firework.pos.x, this.firework.pos.y, false);
+      this.particles.push(p);
+    }
+  }
+
+  show() {
+    if (!this.exploded) {
+      this.firework.show();
+    }
+
+    for (let i = 0; i < this.particles.length; i++) {
+      this.particles[i].show();
+    }
+  }
+}
+
+class Particle {
+  constructor(x, y, firework) {
+    this.pos = createVector(x, y);
+    this.firework = firework;
+    this.lifespan = 255;
+
+    if (this.firework) {
+      this.vel = createVector(0, random(-12, -8));
+    } else {
+      this.vel = p5.Vector.random2D();
+      this.vel.mult(random(2, 10));
+    }
+
+    this.acc = createVector(0, 0);
+    this.hu = random(255);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    if (!this.firework) {
+      this.vel.mult(0.9);
+      this.lifespan -= 4;
+    }
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  done() {
+    return this.lifespan < 0;
+  }
+
+  show() {
+    if (!this.firework) {
+      strokeWeight(2);
+      stroke(this.hu, 255, 255, this.lifespan);
+    } else {
+      strokeWeight(4);
+      stroke(this.hu, 255, 255);
+    }
+
+    point(this.pos.x, this.pos.y);
+  }
+}
