b7ba3fa233fbf341666c339aa9e50f7c5728303e
ogaoga08 <kamekameshin0514@gmail.com>
2024-09-08 17:21:00 +0900

    雛形の完成

--- sketch.js
+++ sketch.js
@@ -1,28 +1,39 @@
-//Real Time Face Detection in p5.js from https://www.youtube.com/watch?v=3yqANLRWGLo
-
 let faceapi;
 let detections = [];
-
 let fireworks = [];
 let gravity;
-
 let video;
 let canvas;
 
-let gameStarted = false; // ゲームが開始されたかどうかのフラグ
-let happy = 0; // happinessの値を保存する変数
+let startButton, hardModeButton, backButton;
+let gameStarted = false;
+let titleVisible = true;
+let timer = 30; // タイマー初期値
+let timerActive = false; // タイマーがアクティブかどうか
+let gameOver = false; // ゲームが終了しているかどうか
+let timerInterval;
+
+// 表情データ
+let neutralG = 0;
+let happyG = 0;
+let angry = 0;
+let sadG = 0;
+let disgustedG = 0;
+let surprisedG = 0;
+let fearfulG = 0;
+
+let shootingRate = 0.1;
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  
   colorMode(HSB);
   gravity = createVector(0, 0.1);
   stroke(255);
   strokeWeight(4);
-  
+
   canvas.id("canvas");
 
-  video = createCapture(VIDEO);// Create video
+  video = createCapture(VIDEO);
   video.id("video");
   video.size(width, height);
 
@@ -33,39 +44,46 @@ function setup() {
     minConfidence: 0.5
   };
 
-  // Initialize the model
   faceapi = ml5.faceApi(video, faceOptions, faceReady);
 
-  // ゲームスタートボタンを作成
-  let startButton = createButton("Start Game");
-  startButton.position(20, 20);
-  startButton.mousePressed(() => {
-    gameStarted = true; // ゲームを開始
-    startButton.hide(); // ボタンを非表示にする
-  });
+  normal();
+  hard();
 }
+
+function normal () {
+  startButton = createButton('Normal Mode');
+  startButton.position(width / 2 - 50, height / 2 + 50);
+  startButton.size(100, 40);
+  startButton.mousePressed(startGame);
+}
+
+function hard () {
+  hardModeButton = createButton('Hard Mode');
+  hardModeButton.position(width / 2 - 50, height / 2 + 100);
+  hardModeButton.size(100, 40);
+  hardModeButton.mousePressed(startGameHardMode);
+} 
 
 function faceReady() {
-  faceapi.detect(gotFaces);// Start detecting faces: 顔認識開始
+  faceapi.detect(gotFaces);
 }
 
-// Get faces
 function gotFaces(error, result) {
   if (error) {
     console.log(error);
     return;
   }
 
-  detections = result;　//Now all the data in this detections: 
+  detections = result;
 
-  clear();//Draw transparent background
-  drawBoxs(detections);//Draw detection box:
-  drawLandmarks(detections);//// Draw all the face points
-  drawExpressions(detections, 20, 250, 14);//Draw face expression
-
-  faceapi.detect(gotFaces);// Call the function again here
+  clear();
+  
+  drawBoxs(detections);
+  drawLandmarks(detections);
+  drawExpressions(detections, 20, 250, 28);
+  
+  faceapi.detect(gotFaces);
 }
-
 function drawBoxs(detections){
   if (detections.length > 0) {//If at least 1 face is detected: 
     for (f=0; f < detections.length; f++){
@@ -96,7 +114,7 @@ function drawExpressions(detections, x, y, textYSpace){
     let {neutral, happy, angry, sad, disgusted, surprised, fearful} = detections[0].expressions;
     textFont('Helvetica Neue');
     textSize(14);
-    
+  
     noStroke();
     fill(0);
 
@@ -108,8 +126,15 @@ function drawExpressions(detections, x, y, textYSpace){
     text("surprised:  " + nf(surprised * 100, 2, 2) + "%", x, y + textYSpace * 5);
     text("fear:           " + nf(fearful * 100, 2, 2) + "%", x, y + textYSpace * 6);
 
-    // happinessの値をグローバル変数に反映
-    happy = happy; 
+    // happiness~fearfulの値をグローバル変数に反映
+    happyG = happy;
+    neutralG = neutral;
+    angerG = angry;
+    sadG = sad;
+    disgustedG = disgusted;
+    surprisedG = surprised;
+    fearfulG = fearful;
+    
   } else { // If no faces are detected
     text("neutral: ", x, y);
     text("happiness: ", x, y + textYSpace);
@@ -121,20 +146,93 @@ function drawExpressions(detections, x, y, textYSpace){
   }
 }
 
-function draw() {
-  if (!gameStarted) return; // ゲームが開始されていない場合は何もしない
+function startGame() {
+  startButton.hide();
+  hardModeButton.hide();
+  titleVisible = false;
+  gameStarted = true;
+  timerActive = true;
+  gameOver = false;
+  timer = 30; // タイマーをリセット
+  fireworks = []; // 花火をリセット
+  startTimer(); // タイマーを開始
+}
 
-  if (random(1) < 0.03) {
-    fireworks.push(new Firework());
+function startGameHardMode() {
+  startGame();
+  // ハードモードの設定をここで追加できます
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    if (timer > 0) {
+      timer--;
+    } else {
+      clearInterval(timerInterval);
+      endGame();
+    }
+  }, 1000);
+}
+
+function endGame() {
+  gameStarted = false;
+  timerActive = false;
+  gameOver = true;
+  clearInterval(timerInterval); // タイマーを停止
+  backButton = createButton('戻る');
+  backButton.position(width / 2 - 50, height / 2 + 50);
+  backButton.size(100, 40);
+  backButton.mousePressed(resetGame);
+}
+
+function resetGame() {
+  backButton.hide();
+  titleVisible = true;
+  gameOver = false;
+  gameStarted = false;
+  fireworks = []; // 花火リセット
+  normal(); // Normalモードボタンを再表示
+  hard(); // Hardモードボタンを再表示
+}
+
+
+function draw() {
+
+  if (titleVisible) {
+    textSize(48);
+    fill(255);
+    textAlign(CENTER);
+    text("花火ゲーム", width / 2, height / 2 - 50);
+    
   }
 
-  for (let i = fireworks.length - 1; i >= 0; i--) {
-    fireworks[i].update();
-    fireworks[i].show();
-
-    if (fireworks[i].done()) {
-      fireworks.splice(i, 1);
+  if (gameStarted) {
+    if (random(1) < shootingRate) {
+      fireworks.push(new Firework());
     }
+
+    for (let i = fireworks.length - 1; i >= 0; i--) {
+      fireworks[i].update();
+      fireworks[i].show();
+
+      if (fireworks[i].done()) {
+        fireworks.splice(i, 1);
+      }
+    }
+
+    if (timerActive) {
+      fill(255);
+      textSize(32);
+      textAlign(RIGHT, TOP);
+      text("Time: " + timer, width - 20, 20); // 画面右上にタイマー表示
+    }
+  }
+
+  if (gameOver) {
+    textSize(48);
+    fill(255);
+    textAlign(CENTER);
+    text("ゲーム終了！", width / 2, height / 2 - 100); // ゲーム終了テキスト表示
   }
 }
 
@@ -153,14 +251,39 @@ class Firework {
     if (!this.exploded) {
       this.firework.applyForce(gravity);
       this.firework.update();
-
-      // happinessが90%以上なら花火を爆発させる
+      gravity = createVector(0, 0.15);
+      shootingRate = 0.1
+      
+      
+      //表情の値で条件分岐
       if (this.firework.vel.y >= 0) {
-        if (happy * 100 >= 90) {
+        if(neutralG * 100 >= 0.90){ // neutralの時、ふつう
+          shootingRate = 0.1;
+        }else if (happyG * 100 >= 0.90) { // happyの時、花火盛大
+          shootingRate = 0.2;
           this.exploded = true;
           this.explode();
-        } else {
-          this.exploded = true; // 爆発はするが、particlesを生成しない
+        } else if (angerG * 100 >= 0.90){ // angerの時、重力なし
+          shootingRate = 0.8;
+          gravity = createVector(0, 0.0);
+        } else if (sadG * 100 >= 0.90){ // sadの時、重力重め
+          shootingRate = 0.07;
+          gravity = createVector(0, 0.3);
+        } else if (disgustedG * 100 >= 0.90){ // disgustedの時、
+          shootingRate = 2;
+          this.exploded = true;
+          this.explode();
+        } else if (surprisedG * 100 >= 0.90){ // surprisedの時、
+          shootingRate = 0.08;
+          this.exploded = true;
+          this.explode();
+        } else if (fearfulG * 100 >= 0.90){ // fearfulの時、
+          shootingRate = 2;
+          this.exploded = true;
+          this.explode();
+        }else {
+          shootingRate = 0.1;
+          this.exploded = true; // 発射はするが、爆発しない
         }
       }
     }
@@ -200,7 +323,7 @@ class Particle {
     this.lifespan = 255;
 
     if (this.firework) {
-      this.vel = createVector(0, random(-12, -8));
+      this.vel = createVector(0, random(-19, -10));
     } else {
       this.vel = p5.Vector.random2D();
       this.vel.mult(random(2, 10));
